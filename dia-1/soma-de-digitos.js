e8d202760216bb1d0077b2fe30bd2a1267b73605
daviazevdo <azvd.davi@gmail.com>
2022-07-05 17:24:07 -0300

    adiciona exercicio soma-de-digitos

--- dia-1/soma-de-digitos.js
+++ dia-1/soma-de-digitos.js
@@ -7,3 +7,21 @@
 // 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 // 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
 
+const digitalRoot = (number) => {
+  let result = Array.from(number.toString());
+
+  while (result.length > 1) {
+    result = result
+      .map((num) => parseFloat(num))
+      .reduce((acc, curr) => acc + curr);
+
+    result = Array.from(result.toString());
+  }
+
+  return parseFloat(result);
+};
+
+// console.log(digitalRoot(16));
+// console.log(digitalRoot(942));
+// console.log(digitalRoot(132189));
+// console.log(digitalRoot(999999999999999));
