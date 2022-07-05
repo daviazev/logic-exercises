b4f2e3e2076bce618dfdd99072b09dcd870c654d
daviazevdo <azvd.davi@gmail.com>
2022-07-05 17:48:42 -0300

    adiciona exercicios ao dia-1

---
+++ dia-1/set-alarm.js
@@ -0,0 +1,10 @@
+// Escreva uma função chamada setAlarm que receba dois parâmetros. O primeiro parâmetro, empregado , é verdadeiro sempre que você estiver empregado e o segundo parâmetro, férias é verdadeiro sempre que você estiver de férias.
+
+// A função deve retornar true se você estiver empregado e não estiver de férias (porque essas são as circunstâncias em que você precisa definir um alarme). Deve retornar false caso contrário. Exemplos:
+
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+// as resoluções dos exercícios podem ser encontrados na branch 'gabarito'
